b0e5740430947d965a4e359ab9701693fd732381
Hanna Yang <hyang6@stuy.edu>
2019-01-02 10:32:24 -0500

    help

--- hannasthingyignore.c
+++ hannasthingyignore.c
@@ -1,47 +1,46 @@
-/*#include <stdlib.h>*/
-/*#include <ctype.h>*/
-/*#include <stdio.h>*/
-/*#include <termios.h>*/
-/*#include <unistd.h>*/
-/*struct termios orig_termios;*/
-/*void disableRawMode() {*/
-  /*tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);*/
-/*}*/
-/*void enableRawMode() {*/
-  /*tcgetattr(STDIN_FILENO, &orig_termios);*/
-  /*atexit(disableRawMode);*/
-  /*struct termios raw = orig_termios;*/
-  /*raw.c_lflag &= ~(ECHO);*/
-  /*tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);*/
-/*}*/
-/*int main() {*/
-  /*enableRawMode();*/
-  /*char c;*/
-  /*while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {*/
-    /*if (iscntrl(c)) {*/
-      /*printf("%d\n", c);*/
-    /*} else {*/
-      /*printf("%d ('%c')\n", c, c);*/
-    /*}*/
-  /*}*/
-  /*return 0;*/
-/*}*/
-
-#include<unistd.h>
-#include<fcntl.h>
-#include<stdio.h>
-
-int main()
-{
-
-if((write(STDIN_FILENO,"arrgh!",6))==-1)
-{
-    printf("error writing to file");
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+struct termios orig_termios;
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO);
+    raw.c_oflag &= ~(OPOST);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+int main() {
+  enableRawMode();
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+  }
+  return 0;
 }
 
-char c;
+/*#include<unistd.h>*/
+/*#include<fcntl.h>*/
+/*#include<stdio.h>*/
 
-read(STDIN_FILENO,&c,10);
-printf("printed: %s\n", &c);
+/*int main()*/
+/*{*/
 
-}
+    /*printf("your current text is: haha\n");*/
+
+    /*printf("haha\r");*/
+
+    /*printf("hm");*/
+    /*char* c[10];*/
+    /*read(STDIN_FILENO, &c, 5);*/
+
+/*}*/
--- makefile
+++ makefile
@@ -4,6 +4,13 @@ all: main.o
 main.o: main.c
 	gcc -c main.c
 
+hannasthingyignore.o: hannasthingyignore.c
+	gcc -c hannasthingyignore.c
+
+hanna: hannasthingyignore.o
+	gcc -o hanna hannasthingyignore.o
+	./hanna
+
 run: 
 	./NAMEHERE
 
