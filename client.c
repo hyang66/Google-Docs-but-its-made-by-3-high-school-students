146e596eafe826985e1e8a6f120572b4166d47e7
MKendall17 <mkendall@stuy.edu>
2019-01-08 17:55:43 -0500

    added to structure and devlog

--- client.c
+++ client.c
@@ -16,13 +16,28 @@ int main() {
   char wr[BUFFER_SIZE];
   char rd[BUFFER_SIZE];
 
-  while(1){
-    printf("[client] enter message to sent to server: ");
+  while(1){ // us sending requests to the server
+    printf("[client] enter line number to start editing:"); 
+    // send line numbers to server, telling which line we are currently editing
     fgets(wr, BUFFER_SIZE, stdin);
     wr[strlen(wr) - 1] = '\0';
     write(to_server, wr, BUFFER_SIZE);
     read(from_server, rd, BUFFER_SIZE);
     printf("[server]: %s\n", rd);
+    // after we recieve the ok from the server,
+    // 
+    // fork off a child to edit line
+    //      PARENT:
+    //          wait for child
+    //          child can return one of arrows or q
+    //              q: 0
+    //              up: 1
+    //              down: 2
+    //          change line number if needed
+    //          if q: done
+    //      CHILD:
+    //          execvp curses and line number
+    //
   }
 
     // server client handshake
