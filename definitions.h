771f4050ce2fec4fc2df18089699814050b4efe2
MKendall17 <mkendall@stuy.edu>
2019-01-09 16:12:27 -0500

    added definitions

---
+++ definitions.h
@@ -0,0 +1,26 @@
+#ifndef DEFINITION
+#define DEFINITION
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+
+//data entry
+#define Q 0
+#define UP  1
+#define DOWN 2
+#define ENTER 3
+
+//linked list
+#define IS_LIST 0
+#define IS_LINE 1
+#define CARGO_MAX 100 
+
+#endif
