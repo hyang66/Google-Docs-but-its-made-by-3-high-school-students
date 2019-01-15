6ab556ca2a1c37b83e5c2871db4d64f433b68b3c
MKendall17 <mkendall@stuy.edu>
2019-01-15 09:26:52 -0500

    fff

---
+++ code.c
@@ -0,0 +1,20 @@
+            ch = getch();
+            if (ch == 'q' || ch == 'D' || ch == 'U'){
+                break;
+            }
+
+
+
+if (ch == 'Q') {
+        exit(Q);
+} 
+if (ch == 'U') {
+        exit(UP);
+} 
+if (ch == 'D') {
+        exit(DOWN);
+} 
+
+
+                     { KEY_ENTER,     "ENTER"         },
+
